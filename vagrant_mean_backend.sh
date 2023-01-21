ed9d74a863373aa26e92898f3967d8703637f7ca
ashrafmuqlis <ashrafmuqlis@gmail.com>
2023-01-21 22:01:38 +0800

    Update vagrant_mean_backend.sh

--- vagrant_mean_backend.sh
+++ vagrant_mean_backend.sh
@@ -47,7 +47,7 @@ require(\"dotenv\").config({ path: \"./config.env\" });
 const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
-app.use(require(\"./routes/record\"));
+app.use(require(\"./routes/employees\"));
 // get driver connection
 const dbo = require(\"./db/conn\");
  
