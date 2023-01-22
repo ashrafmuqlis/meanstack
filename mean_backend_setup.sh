7b0205966b498160388c7e8b7cd025cc9dcf1044
ashrafmuqlis <ashrafmuqlis@gmail.com>
2023-01-22 21:34:27 +0800

    Update mean_backend_setup.sh

--- mean_backend_setup.sh
+++ mean_backend_setup.sh
@@ -48,8 +48,7 @@ app.use((req, res, next) => {
 app.use(function (err, req, res, next) {
   console.error(err.message) // Log error message in our server's console
   if (!err.statusCode) err.statusCode = 500 // If err has no specified error code, set error code to 'Internal Server Error (500)'    
-  res.status(err.statusCode).send(err.message) // All HTTP requests must have a response, so let's send back an error with its status 
-code and message
+  res.status(err.statusCode).send(err.message) // All HTTP requests must have a response, so let's send back an error with its status code and message
 })" > server.js
 
 sudo sed -i 's/index.js/server.js/' package.json
