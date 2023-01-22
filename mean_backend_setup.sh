fb966bb6f189a987ab536c770191b04ac00d82bc
ashrafmuqlis <ashrafmuqlis@gmail.com>
2023-01-22 22:51:56 +0800

    Update mean_backend_setup.sh

--- mean_backend_setup.sh
+++ mean_backend_setup.sh
@@ -17,7 +17,7 @@ const bodyParser = require('body-parser')
 mongoose
   .connect('mongodb://localhost:27017/mydatabase')
   .then((x) => {
-    console.log(`Connected to Mongo!`)
+    console.log('Connected to Mongo!')
   })
   .catch((err) => {
     console.error('Error connecting to mongo', err.reason)
