0eebab3ce3c4f44fcb4f509e05a820a5f5585eee
ashrafmuqlis <ashrafmuqlis@gmail.com>
2023-01-22 21:38:13 +0800

    Update mean_backend_setup.sh

--- mean_backend_setup.sh
+++ mean_backend_setup.sh
@@ -119,7 +119,7 @@ employeeRoute.route('/read/:id').get((req, res) => {
 // Update employee
 employeeRoute.route('/update/:id').put((req, res, next) => {
   Employee.findByIdAndUpdate(req.params.id, {
-    $set: req.body
+    \$set: req.body
   }, (error, data) => {
     if (error) {
       return next(error);
