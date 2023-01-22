05b91910c17eadacbbbd2a46651e2bd6bf8bc996
ashrafmuqlis <ashrafmuqlis@gmail.com>
2023-01-22 23:49:14 +0800

    Update mean_backend_setup.sh

--- mean_backend_setup.sh
+++ mean_backend_setup.sh
@@ -17,7 +17,7 @@ const bodyParser = require('body-parser')
 mongoose
   .connect('mongodb://localhost:27017/mydatabase')
   .then((x) => {
-    console.log('Connected to Mongo!')
+    console.log(Connected to Mongo! Database name: \"${x.connections[0].name}\")
   })
   .catch((err) => {
     console.error('Error connecting to mongo', err.reason)
