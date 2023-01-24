ca41adc106207da1f61b64947ae2c5739febfa39
ashrafmuqlis <ashrafmuqlis@gmail.com>
2023-01-24 20:34:26 +0800

    Update backend_setup.sh

--- backend_setup.sh
+++ backend_setup.sh
@@ -79,7 +79,7 @@ db.mongoose
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log();
 });" > server.js
 
 sudo sed -i 's/console.log()/console.log(`Server is running on port ${PORT}.`)/' server.js
